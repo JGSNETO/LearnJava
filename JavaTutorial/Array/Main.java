85b42ae6e3a8049c46f30ef2775deb2f301fdb7e
José Neto <jgsnto.sd@gmail.com>
2022-03-14 06:47:45 -0300

    Adding: Array; ForLoop; IfEsle ; Switch; Whileloop

---
+++ JavaTutorial/Array/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] carro = { "Volvo", "Fox" };
+        carro[0] = "Fiat";
+
+        for (int i = 0; i < carro.length; ++i) {
+            System.out.println(carro[i]);
+        }
+        int[][] muNumbers = { { 1, 2, 3 }, { 5, 6, 7 } };
+    }
+}
---
+++ JavaTutorial/IfElse/Main.java
@@ -0,0 +1,25 @@
+/**
+ * If
+ * Else
+ * Elseif
+ * Switch
+ * 
+ */
+public class Main {
+    public static void main(String[] args) {
+        int time = 22;
+        // Short hand if esle
+        String resulta = (time < 10) ? "Good day" : "Good night";
+        System.out.println(resulta);
+        /**
+         * if(time < 10){
+         * System.out.println("Good moorning");
+         * } else if (time < 20){
+         * System.out.println("Good day");
+         * } else {
+         * System.out.println("Good night");
+         * }
+         */
+
+    }
+}
